8cca27322cc534087921c3d4ce7ffcdd88c5cfd4
antonjaragon <antonjaragon@gmail.com>
2023-09-13 22:04:39 +0200

    web recorder and example of pipeline

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        # if "file" not in request.files:
+        #     return redirect(request.url)
+
+        # file = request.files["file"]
+        # if file.filename == "":
+        #     return redirect(request.url)
+
+        # if file:
+        #     recognizer = sr.Recognizer()
+        #     audioFile = sr.AudioFile(file)
+        #     with audioFile as source:
+        #         data = recognizer.record(source)
+        #     transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('index.html', transcript=transcript)
+
 
 @app.route('/cache-me')
 def cache():
