9bac7d6d779283eb02b995655cd66b44c1b4f76c
antonjaragon <antonjaragon@gmail.com>
2023-09-22 23:43:12 +0200

    dashboard auth and folders clean up

--- basic/run.sh
+++
@@ -1,5 +0,0 @@
-openssl req -x509 -nodes -newkey rsa:2048 -keyout nginx/key.pem -out nginx/cert.pem -sha256 -days 365 \
-	-subj "/C=ES/ST=Seville/L=Seville/O=US/OU=IT/CN=localhost"
-
-
-docker compose up --build
